732a2edd679386e416a9f261b9a2e6a7adbc639c
bunnie <bunnie@kosagi.com>
2022-04-20 16:20:00 +0100

    commit v0.9.8 snapshot artifacts

--- precursors/fetch_binaries.sh
+++ precursors/fetch_binaries.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 set -e
 
-echo "Updating to v0.9.5 stabilized release"
+read -p "Download $1 artifacts? (y/N) " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
 
-wget --no-check-certificate  https://ci.betrusted.io/releases/v0.9.5/loader.bin -O loader.bin
+echo "Updating to $1 stabilized release"
 
-wget --no-check-certificate  https://ci.betrusted.io/releases/v0.9.5/xous.img -O xous.img
+wget --no-check-certificate  https://ci.betrusted.io/releases/$1/loader.bin -O loader.bin
 
-wget --no-check-certificate  https://ci.betrusted.io/releases/v0.9.5/soc_csr.bin -O soc_csr.bin
+wget --no-check-certificate  https://ci.betrusted.io/releases/$1/xous.img -O xous.img
 
-wget --no-check-certificate  https://ci.betrusted.io/releases/v0.9.5/ec_fw.bin -O ec_fw.bin
-wget --no-check-certificate  https://ci.betrusted.io/releases/v0.9.5/bt-ec.bin -O bt-ec.bin
-wget --no-check-certificate  https://ci.betrusted.io/releases/v0.9.5/wfm_wf200_C0.sec -O wfm_wf200_C0.sec
-wget --no-check-certificate  https://ci.betrusted.io/releases/v0.9.5/wf200_fw.bin -O wf200_fw.bin
+wget --no-check-certificate  https://ci.betrusted.io/releases/$1/soc_csr.bin -O soc_csr.bin
+
+wget --no-check-certificate  https://ci.betrusted.io/releases/$1/ec_fw.bin -O ec_fw.bin
+wget --no-check-certificate  https://ci.betrusted.io/releases/$1/bt-ec.bin -O bt-ec.bin
+wget --no-check-certificate  https://ci.betrusted.io/releases/$1/wfm_wf200_C0.sec -O wfm_wf200_C0.sec
+wget --no-check-certificate  https://ci.betrusted.io/releases/$1/wf200_fw.bin -O wf200_fw.bin
+
+echo $1 > VERSION
